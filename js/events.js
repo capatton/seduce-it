4c80a74215593e4b5c3fe3ca037398806328f192
caPatton <casey.a.patton@gmail.com>
2013-04-13 08:05:12 -0700

    Got the events queries done

--- js/events.js
+++ js/events.js
@@ -1,3 +1,4 @@
+var eventDataArray = [];
 window.fbAsyncInit = function() {
   FB.init({
           appId      : '452746094806213', // App ID
@@ -6,6 +7,22 @@ window.fbAsyncInit = function() {
           cookie     : true, // enable cookies to allow the server to access the session
           xfbml      : true  // parse XFBML
         });
+  FB.getLoginStatus(function(response){
+    if (response.status === 'connected') {
+      //name, date, friends attending, link
+    var getEventsOfCrush = 'SELECT name, privacy, start_time, eid FROM event WHERE eid IN (SELECT eid FROM event_member WHERE uid = 744778013 AND rsvp_status = "attending" AND start_time > now()) AND privacy="OPEN" LIMIT 3';
+    FB.api('/fql', 'GET', {q: getEventsOfCrush}, function(response) {
+    if (response && response.data) {
+        for (var i = 0; i < response.data.length; ++i)
+        {
+          eventDataArray.push([response.data[i].name, response.data[i].start_time, response.data[i].eid]);
+        }
+    }
+    //NICK: At this point, eventDataArray has the data stored as tuples - {name, start_time, eid}
+
+  })}});     
+
+};
 
 // Load the SDK Asynchronously
 (function(d){
@@ -15,3 +32,5 @@ window.fbAsyncInit = function() {
   js.src = "//connect.facebook.net/en_US/all.js";
   ref.parentNode.insertBefore(js, ref);
 }(document));
+
+
