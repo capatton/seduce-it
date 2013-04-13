d36e93ec81c2b8127e2b3d18392689e882185f8b
Nick Van Hoogenstyn <nickvanhoog@gmail.com>
2013-04-12 22:04:13 -0700

    Removed post

--- check.py
+++ check.py
@@ -21,23 +21,8 @@ class CheckPage(webapp2.RequestHandler):
         if user is None:
             new_profile = UserProfile(user_id = user_id)
             new_profile.put()
-            #self.redirect('/choose/{}'.format(user_id))
+            self.redirect('/choose/{}'.format(user_id))
         else:
-            self.response.headers['Content-Type'] = 'text/html'
-            path = os.path.join(os.path.dirname(__file__), 'check.html')
-            self.response.out.write(template.render(path, {'user_name': user.user_name, 'user_id': user.user_id}))
-            #self.redirect('/dashboard/{}'.format(user_id))
-
-    def post(self):
-        user_id = self.request.get('id')
-        user_name = self.request.get('name')
-
-        q = db.Query(UserProfile)
-        q.filter('user_id = ', user_id)
-
-        user = q.get()
-        if user is None:
-            new_profile = UserProfile(user_id = user_id, user_name = user_name)
-            new_profile.put()
+            self.redirect('/dashboard/{}'.format(user_id))
 
 app = webapp2.WSGIApplication([('/check/(\d*)', CheckPage)], debug=True)
--- js/fblogin.js
+++ js/fblogin.js
@@ -14,8 +14,7 @@ window.fbAsyncInit = function() {
         FB.api('/fql', 'GET', {q: getNameQuery}, function(response) {
           if (response && response.data) {
               console.log("CONNECTED");
-              $.post("check", { id: userIdString, name: response.data[0].name });
-              window.location = "check";
+              window.location = "check/" + userIdString;
           }     
       });
   });
